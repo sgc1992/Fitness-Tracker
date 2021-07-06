d81783f1b8b88f23b475e15fe8088ecb5c60376d
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-06 18:21:04 +1000

    added the modules and git ignore file

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+// Import Express router
+const router = require("express").Router();
+
+//Import path
+const path = require("path");
+
+// GET Request : Exercise Page
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// GET Request : Stats Page
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// Export HTML routes
+module.exports = router;
